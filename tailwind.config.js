1e8ce68cf62e605ea71aca81281d118ff9e64e7b
Igor Babko <i.i.babko@studnote.com>
2021-11-17 13:42:39 +0200

    7: Responsive playlists grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  theme: {
+    screens: {
+      'sm': '500px',
+      'md': '900px',
+      'lg': '1100px',
+      'xl': '1400px',
+      '2xl': '1600px',
+      '3xl': '1800px',
+      '4xl': '2000px',
+      '5xl': '2200px'
+    },
+    extend: {
+      gridTemplateColumns: {
+        'main': 'repeat(auto-fit, minmax(180px, 1fr))'
+      }
+    }
+  },
   plugins: [
     require('@tailwindcss/line-clamp'),
   ],
