d07b5f4ff2fa2fda666cfc5cedb2b30814e1d69e
Igor Babko <i.i.babko@studnote.com>
2021-11-20 12:09:45 +0200

    8: Improve playlists grid responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,20 @@ module.exports = {
   theme: {
     screens: {
       'sm': '500px',
-      'md': '900px',
-      'lg': '1100px',
-      'xl': '1400px',
-      '2xl': '1600px',
-      '3xl': '1800px',
-      '4xl': '2000px',
-      '5xl': '2200px'
+      'md': '700px',
+      'lg': '900px',
+      'xl': '1100px',
+      '2xl': '1300px',
+      '3xl': '1500px',
+      '4xl': '1700px',
+      '5xl': '1900px',
+      '6xl': '2100px'
     },
     extend: {
       gridTemplateColumns: {
-        'main': 'repeat(auto-fit, minmax(180px, 1fr))'
+        'playlists-mobile': 'repeat(auto-fill, minmax(208px, 1fr))',
+        'playlists-tablet': 'repeat(auto-fill, minmax(266px, 1fr))',
+        'playlists-desktop': 'repeat(auto-fill, minmax(180px, 1fr))',
       }
     }
   },
